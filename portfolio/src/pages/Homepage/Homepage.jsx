183949b07d282a12cbd7f129f8cdcb404404265f
Your Name <you@example.com>
2025-04-09 13:01:46 -0700

    finished hero section

--- portfolio/src/pages/Homepage/Homepage.jsx
+++ portfolio/src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import "./Homepage.scss";
 import Navigation from "../../components/Navigation/Navigation";
+import Hero from "../../components/Hero/Hero";
 
 export default function Homepage() {
   return (
@@ -8,6 +9,7 @@ export default function Homepage() {
         <nav>
           <Navigation />
         </nav>
+        <Hero />
       </header>
       <main>
 
