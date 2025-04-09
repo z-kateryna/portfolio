27fea407fdc13c6790a0b73c4d6b2ab9e280582e
Your Name <you@example.com>
2025-04-09 08:42:42 -0700

    folder structure set up

--- portfolio/src/pages/Homepage/Homepage.jsx
+++ portfolio/src/pages/Homepage/Homepage.jsx
@@ -1,29 +1,20 @@
 import "./Homepage.scss";
-import arrow from '../../assets/icons/arrow.svg';
-import dribble from '../../assets/icons/dribbble.svg';
-import github from '../../assets/icons/github.svg';
-import linkedin from '../../assets/icons/linkedIn.svg';
-import mail from '../../assets/icons/mail.svg';
+import Navigation from "../../components/Navigation/Navigation";
 
 export default function Homepage() {
-    return (
-        <>
-        <nav className="nav">
-            <p>K</p>
-            <ul>
-                <li><a href="#"></a>Home</li>
-                <li><a href="#"></a>About</li>
-                <li><a href="#"></a>Coding<img src={arrow}/></li>
-                <li><a href="#"></a>Resume<img src={arrow}/></li>
-            </ul>
-            <div>
-                <a href="#"><img src={dribble} alt="github icon"/></a>
-                <a href="#"><img src={github} alt="github icon"/></a>
-                <a href="#"><img src={linkedin} alt="github icon"/></a>
-                <a href="#"><img src={mail} alt="github icon"/></a>
-            </div>
+  return (
+    <>
+      <header>
+        <nav>
+          <Navigation />
         </nav>
-        <p>Hi, I'm Kat</p>
-        </>
-    )
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </>
+  );
 }
