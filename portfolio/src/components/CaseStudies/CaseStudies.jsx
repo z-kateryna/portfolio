c3a901268848487f91041cc81a721b11d09b1c78
Your Name <you@example.com>
2025-04-10 18:20:45 -0700

    added references section

--- portfolio/src/components/CaseStudies/CaseStudies.jsx
+++ portfolio/src/components/CaseStudies/CaseStudies.jsx
@@ -19,10 +19,10 @@ export default function CaseStudies() {
           <div className="case-studies__copy-container">
             <h2 className="case-studies__header">Designing for Impact: Launching Pyrukynd's Digital Presence</h2>
             <p className="case-studies__copy">This case study explores the design and development process behind the Pyrukynd website, focusing on creating an engaging, user-friendly digital resource for patients and healthcare providers, aimed at delivering crucial treatment information with clarity and ease.</p>
-            <button className="case-studies__btn">
+            {/* <button className="case-studies__btn">
               read case study
               <img src={Arrow} />
-            </button>
+            </button> */}
           </div>
         </a>
         <a className="case-studies__myagios" href="#">
@@ -35,10 +35,10 @@ export default function CaseStudies() {
           <div className="case-studies__copy-container">
             <h2 className="case-studies__header">Title</h2>
             <p className="case-studies__copy">Overview</p>
-            <button className="case-studies__btn">
+            {/* <button className="case-studies__btn">
               Read Case Study
               <img src={Arrow} />
-            </button>
+            </button> */}
           </div>
         </a>
         <a className="case-studies__nowsta" href="#">
@@ -51,10 +51,10 @@ export default function CaseStudies() {
           <div className="case-studies__copy-container">
             <h2 className="case-studies__header">Title</h2>
             <p className="case-studies__copy">Overview</p>
-            <button className="case-studies__btn">
+            {/* <button className="case-studies__btn">
               Read Case Study
               <img src={Arrow} />
-            </button>
+            </button> */}
           </div>
         </a>
         <a className="case-studies__cob" href="#">
@@ -67,10 +67,10 @@ export default function CaseStudies() {
           <div className="case-studies__copy-container">
             <h2 className="case-studies__header">Title</h2>
             <p className="case-studies__copy">Overview</p>
-            <button className="case-studies__btn">
+            {/* <button className="case-studies__btn">
               Read Case Study
               <img src={Arrow} />
-            </button>
+            </button> */}
           </div>
         </a>
       </section>
--- portfolio/src/pages/Homepage/Homepage.jsx
+++ portfolio/src/pages/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import "./Homepage.scss";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
 import CaseStudies from "../../components/CaseStudies/CaseStudies";
+import Comments from "../../components/Comments/Comments";
 
 export default function Homepage() {
   return (
@@ -14,7 +15,7 @@ export default function Homepage() {
       </header>
       <main>
         <CaseStudies />
-
+        <Comments />
       </main>
       <footer>
         
